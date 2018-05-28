0a53cf54c0dce8c1b6c82e0f822b165539c6c7cd
ViktorBasanets <vbasanec@gmail.com>
2018-05-28 17:09:14 +0300

    The last release

--- src/Task1/UnixPath.java
+++ src/Task1/UnixPath.java
@@ -3,31 +3,20 @@ package Task1;
 import java.util.Stack;
 
 public class UnixPath {
-    char[] path;
-    Stack<char[]> stack;
-    char[] resultPath;
+    private char[] path;
+    private Stack<char[]> stack;
+    private char[][] result;
 
     public UnixPath(char[] path) {
         this.path = path;
-        stack = new Stack<>();
-    }
-
-    public char[] getResultPath() {
-
-        return resultPath;
+        this.stack = new Stack<>();
     }
 
     public UnixPath simplifies() {
-        resultPath = new char[1];
-        resultPath[0] = '/';
-
         for(int i = 0; i < path.length; i++) {
-
             String tempLink = "";
 
-            while (path[i] == '/') {
-                i++;
-            }
+            while (path[i] == '/') { i++; }
 
             while (i < path.length && path[i] != '/') {
                 tempLink += path[i++];
@@ -38,45 +27,23 @@ public class UnixPath {
                     stack.pop();
             } else if (tempLink.equals(".")) {
                 continue;
-            } else if (tempLink.length() != 0) {
-                stack.push(tempLink.toCharArray());
+            } else {
+                stack.push(("/" + tempLink).toCharArray());
             }
         }
 
-        reverseToResult();
         return this;
     }
 
-    private void reverseToResult() {
-        Stack<char[]> tempStack = new Stack<>();
+    public void showResult() {
+        result = stack.toArray(new char[0][]);
 
-        while (!stack.isEmpty()) {
-            tempStack.push(stack.pop());
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i]);
         }
 
-        while (!tempStack.isEmpty()) {
-            char[] array = tempStack.peek();
-
-            if (tempStack.size() != 1) {
-                saveArray(array,resultPath.length + array.length + 1);
-                resultPath[resultPath.length - 1] = '/';
-            } else {
-                saveArray(array,resultPath.length + array.length);
-            }
-            tempStack.pop();
-        }
-    }
-
-    private void saveArray(char[] tempArray, int length) {
-        char[] temp = new char[length];
-        int i;
-        for (i = 0; i < resultPath.length; i++) {
-            temp[i] = resultPath[i];
-        }
-
-        for (int j = 0; j < tempArray.length; j++, i++) {
-            temp[i] = tempArray[j];
-        }
-        resultPath = temp;
+        if (result.length == 0) {
+            System.out.println("/");
+        } else System.out.println();
     }
 }
